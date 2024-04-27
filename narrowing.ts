2568ffd9d66dde468fa9d86c2fda2a1c6c9cba5c
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-27 21:24:25 +0530

    feat: add more ts examples

--- narrowing.ts
+++ narrowing.ts
@@ -170,8 +170,23 @@ const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
   return isFish(pet);
 });
 
-interface Shape {
-  kind: "circle"|"square";
-  radius?: number;
-  sideLength?: number;
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  sideLength: number;
+}
+
+type Shape = Circle|Square;
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+  case "circle":
+    return Math.PI * shape.radius ** 2;
+  case "square":
+    return shape.sideLength ** 2;
+  }
 }
