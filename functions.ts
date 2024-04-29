7eeb6282e52d4e73ccd6e7c52d88b39305bcbfac
Linus Fernandes <linus.fernandes@gmail.com>
2024-04-29 20:08:03 +0530

    feat: add more ts examples

--- functions.ts
+++ functions.ts
@@ -112,3 +112,46 @@ function geet<Str extends string>(s: Str) { console.log("Hello, " + s); }
 geet("world");
 
 function get(s: string) { console.log("Hello, " + s); }
+
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i);
+  }
+}
+
+myForEach([ 1, 2, 3 ], (a) => console.log(a));
+myForEach([ 1, 2, 3 ], (a, i) => console.log(a, i));
+myForEach([ 1, 2, 3 ], (a, i) => { console.log(i?.toFixed()); });
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+
+function fnn(x: boolean): void;
+function fnn(x: boolean) {}
+function fna(x: string): string;
+function fna(x: string|number) { return "oops"; }
+
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(arr: any[]|string): number;
+function len(x: any) { return x.length; }
+
+len("");    // OK
+len([ 0 ]); // OK
+len(Math.random() > 0.5 ? "hello" : [ 0 ]);
+
+const user = {
+  id : 123,
+
+  admin : false,
+  becomeAdmin : function() { this.admin = true; },
+};
